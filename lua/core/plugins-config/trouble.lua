5fd6e9c272d0ddf858201281248db488e1455805
Uladzsislau Hotsin <uladzislau.hotsin@limango.com>
2024-06-29 20:44:02 +0200

    feat(cmp): completions

--- lua/core/plugins-config/trouble.lua
+++ lua/core/plugins-config/trouble.lua
@@ -1,8 +1,6 @@
-local map = vim.api.nvim_set_keymap
+local api = vim.api
+local map = api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map("n", "<leader>to", "<cmd>lua require('trouble').open()<cr>", opts)
-
-map("n", "<leader>tt", "<cmd>lua require('trouble').toggle()<cr>", opts)
-
-map("n", "<leader>tc", "<cmd>lua require('trouble').close()<cr>", opts)
+-- Trouble mappings
+map("n", "<leader>tt", "<cmd>lua require('trouble').toggle({mode='diagnostics'})<cr>", opts)

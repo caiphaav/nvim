8d581ce7d6c564323fbd05efd0db43e066d622e4
uhotsin <uladgotin@gmail.com>
2024-09-15 00:28:34 +0200

    feat(which_key): config

--- lua/core/plugins-config/telescope.lua
+++ lua/core/plugins-config/telescope.lua
@@ -59,6 +59,7 @@ end, { desc = "[/] Fuzzily search in current buffer" })
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
-vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+  { desc = "[F]ind [G]rep" })
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
--- lua/core/plugins-config/trouble.lua
+++ lua/core/plugins-config/trouble.lua
@@ -1,6 +1,6 @@
 local api = vim.api
 local map = api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true, desc = "Toggle Trouble" }
 
 -- Trouble mappings
 map("n", "<leader>tt", "<cmd>lua require('trouble').toggle({mode='diagnostics'})<cr>", opts)

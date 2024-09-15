9c438d45db885408ceafa48fb867e26a5d3ba62e
uhotsin <uladgotin@gmail.com>
2024-09-16 00:03:22 +0200

    feat(telescope): shortcut

--- lua/core/plugins-config/telescope.lua
+++ lua/core/plugins-config/telescope.lua
@@ -36,6 +36,20 @@ local function paste_file(prompt_bufnr)
   end
 end
 
+local function open_telescope_file_browser()
+  local current_buffer_file_path = vim.fn.expand("%:p:h")
+  require("telescope").extensions.file_browser.file_browser({
+    path = current_buffer_file_path,
+    cwd = current_buffer_file_path,
+    respect_gitignore = false,
+    hidden = true,
+    grouped = true,
+    previewer = false,
+    initial_mode = "normal",
+    layout_config = { height = 40 }
+  })
+end
+
 telescope.setup({
   defaults = {
     file_ignore_patterns = {
@@ -116,16 +130,5 @@ vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_gre
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
 vim.keymap.set("n", "<space>fa", ":Telescope file_browser<CR>", { desc = "[F]ind [B]rowser" })
-vim.keymap.set("n", "<space>fb", function()
-  local current_buffer_file_path = vim.fn.expand("%:p:h")
-  require("telescope").extensions.file_browser.file_browser({
-    path = current_buffer_file_path,
-    cwd = current_buffer_file_path,
-    respect_gitignore = false,
-    hidden = true,
-    grouped = true,
-    previewer = false,
-    initial_mode = "normal",
-    layout_config = { height = 40 }
-  })
-end, { desc = "[F]ile [B]rowser in current directory" })
+vim.keymap.set("n", "f", open_telescope_file_browser, { desc = "[F]ile [B]rowser in current directory" })
+vim.keymap.set("n", "<space>fb", open_telescope_file_browser, { desc = "[F]ile [B]rowser in current directory" })
